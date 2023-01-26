96ba7b286c4ceec100fe4d7b5228cbd45db440df
George Gillams <code@georgegillams.co.uk>
2023-01-26 20:11:44 +0000

    fix: Initial commit

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # network-monitor
+
+A simple server application which can be run on a raspberry pi to monitor network up/down status.
+
+# Local development
+
+- Install dependencies using `yarn`.
+- Run the server using `yarn start`.
+- Visit http://192.168.0.95:8080/logs
+
+# Deployment
+
+- Run yarn
+- Configure the Pi to run `yarn start` on boot using PM2, crontab, or similar.
---
+++ index.js
@@ -0,0 +1,85 @@
+const { execSync } = require("child_process");
+const fs = require("fs");
+const http = require("http");
+
+const LOG_FILE = "../network_monitor_log.txt";
+const SECONDS_60 = 60 * 1000;
+const TIME_BETWEEN_CHECKS = SECONDS_60;
+const HOURS_1 = 60 * 60 * 1000;
+const TIME_BETWEEN_SPEED_TESTS = HOURS_1;
+
+let lastStatus = null;
+
+const waitFor = async (ms) => {
+  return new Promise((resolve) => setTimeout(resolve, ms));
+};
+
+const requestListener = async (req, res) => {
+  if (req.url === "/logs") {
+    const logs = fs.readFileSync(LOG_FILE, { encoding: "utf-8" });
+    res.writeHead(200);
+    res.end(logs);
+  } else {
+    res.writeHead(404);
+    res.end("Not found");
+  }
+};
+
+const logNetworkStatus = (status) => {
+  if (lastStatus !== status) {
+    lastStatus = status;
+
+    fs.appendFileSync(
+      LOG_FILE,
+      `${new Date().toISOString()} NETWORK ${status}\n`
+    );
+  }
+};
+
+const logNetworkUp = () => {
+  logNetworkStatus("UP");
+};
+
+const logNetworkDown = () => {
+  logNetworkStatus("DOWN");
+};
+
+const checkNetwork = async () => {
+  try {
+    execSync("ping -c 3 google.com");
+    logNetworkUp();
+  } catch (error) {
+    logNetworkDown();
+  }
+};
+
+const runSpeedTest = () => {
+  try {
+    const speedTestResults = execSync("speedtest-cli --simple").toString();
+    const resultsString = speedTestResults.split("\n").join(" ");
+    fs.appendFileSync(
+      `${LOG_FILE}`,
+      `${new Date().toISOString()} ${resultsString}\n`
+    );
+  } catch (error) {
+    fs.appendFileSync(
+      `${LOG_FILE}`,
+      `${new Date().toISOString()} Speed test failed\n`
+    );
+  }
+};
+
+setInterval(() => {
+  runSpeedTest();
+}, TIME_BETWEEN_SPEED_TESTS);
+
+setInterval(() => {
+  checkNetwork();
+}, TIME_BETWEEN_CHECKS);
+
+const server = http.createServer();
+server.on("request", requestListener);
+
+server.listen(process.env.PORT || 8080);
+
+fs.appendFileSync(LOG_FILE, `${new Date().toISOString()} SERVER RUNNING\n`);
